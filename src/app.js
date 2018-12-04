e11ff4de985e58dd5e7c671fa818082773c1ce2d
Maciej Modliński <maciej.modlinski@veneo.eu>
2018-12-04 11:44:29 +0100

    add popup and menu

--- src/app.js
+++ src/app.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import Events from './events/Events';
 import Launches from './launches/Launches';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
+import {BrowserRouter as Router, Route, Switch, NavLink} from 'react-router-dom'
 
 const App = () => {
-    return (<Router>
-        <div className="">
-            <Switch>
-                <Route path="/launche/:launcheId" component={Launches}/>
-                <Route exact path="/" component={Events}/>
-            </Switch>
-        </div>
-    </Router>);
+    return (
+        <Router>
+            <div>
+                <header>
+                    <ul>
+                      <li>
+                        <NavLink exact to="/" activeStyle={{ fontWeight: 'bold' }}>Home</NavLink>
+                      </li>
+                      <li>
+                        <NavLink to="/about" activeStyle={{ fontWeight: 'bold' }}>O stronie</NavLink>
+                      </li>
+                    </ul>
+                </header>
+                <div className="content_page">
+                    <Switch>
+                        <Route exact path="/" component={Events}/>
+                        <Route path="/launche/:launcheId" component={Launches}/>
+                        <Route path="/about" render={() => <p>O strnie</p>} />
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
